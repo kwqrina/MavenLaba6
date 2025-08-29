49135182a77c034675e4b9d9226c46c4ff5528c3
kwqrina <ekaterina.motruk@gmail.com>
2025-08-29 12:37:21 +0300

    1

---
+++ mavenlaba7/src/main/java/com/mycompany/mavenlaba7/Mavenlaba7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenlaba7;
+
+/**
+ *
+ * @author ekate
+ */
+public class Mavenlaba7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
